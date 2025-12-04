e66a2b8a63f88d0c66daf81d5457031545a2307f
vertexcubed <95980769+vertexcubed@users.noreply.github.com>
2025-12-04 16:52:07 -0500

    Initial Commit

---
+++ src/combinator.rs
@@ -0,0 +1,64 @@
+use crate::{Bop, Expr, Literal};
+
+
+
+
+pub fn var(name: &str) -> Expr {
+    Expr::Ident(name.to_string())
+}
+pub fn num(num: i64) -> Expr {
+    Expr::Lit(Literal::Int(num))
+}
+pub fn bool(value: bool) -> Expr {
+    Expr::Lit(Literal::Bool(value))
+}
+pub fn unit() -> Expr {
+    Expr::Lit(Literal::Unit)
+}
+
+pub fn let_(binding: &str, body: Expr, rest: Expr) -> Expr {
+    Expr::Let(binding.to_string(), Box::new(body), Box::new(rest))
+}
+impl Expr {
+    pub fn add(self, rhs: Expr) -> Expr {
+        Expr::BinOp(Bop::Add, Box::new(self), Box::new(rhs))
+    }
+    pub fn sub(self, rhs: Expr) -> Expr {
+        Expr::BinOp(Bop::Sub, Box::new(self), Box::new(rhs))
+    }
+    pub fn mul(self, rhs: Expr) -> Expr {
+        Expr::BinOp(Bop::Mul, Box::new(self), Box::new(rhs))
+    }
+    pub fn div(self, rhs: Expr) -> Expr {
+        Expr::BinOp(Bop::Div, Box::new(self), Box::new(rhs))
+    }
+    pub fn lt(self, rhs: Expr) -> Expr {
+        Expr::BinOp(Bop::Lt, Box::new(self), Box::new(rhs))
+    }
+    pub fn lte(self, rhs: Expr) -> Expr {
+        Expr::BinOp(Bop::Lte, Box::new(self), Box::new(rhs))
+    }
+    pub fn gt(self, rhs: Expr) -> Expr {
+        Expr::BinOp(Bop::Gt, Box::new(self), Box::new(rhs))
+    }
+    pub fn gte(self, rhs: Expr) -> Expr {
+        Expr::BinOp(Bop::Gte, Box::new(self), Box::new(rhs))
+    }
+    pub fn eq(self, rhs: Expr) -> Expr {
+        Expr::BinOp(Bop::Eq, Box::new(self), Box::new(rhs))
+    }
+    pub fn neq(self, rhs: Expr) -> Expr {
+        Expr::BinOp(Bop::Neq, Box::new(self), Box::new(rhs))
+    }
+
+}
+
+pub fn if_(cond: Expr, then: Expr, else_: Expr) -> Expr {
+    Expr::If(Box::new(cond), Box::new(then), Box::new(else_))
+}
+pub fn func(name: Option<&str>, arg: &str, body: Expr) -> Expr {
+    Expr::Closure(name.map(|s| s.to_string()), arg.to_string(), Box::new(body))
+}
+pub fn app(func: Expr, arg: Expr) -> Expr {
+    Expr::App(Box::new(func), Box::new(arg))
+}
---
+++ src/main.rs
@@ -0,0 +1,147 @@
+use std::collections::{BTreeMap, HashMap};
+use crate::combinator::*;
+
+mod combinator;
+
+type Identifier = String;
+
+#[derive(Debug)]
+enum Expr {
+    Lit(Literal),
+    Ident(Identifier),
+    // let x = 2 in ...
+    Let(Identifier, Box<Expr>, Box<Expr>),
+    BinOp(Bop, Box<Expr>, Box<Expr>),
+    If(Box<Expr>, Box<Expr>, Box<Expr>),
+    Closure(Option<Identifier>, Identifier, Box<Expr>),
+    App(Box<Expr>, Box<Expr>),
+}
+#[derive(Debug, Copy, Clone)]
+enum Bop {
+    Add,
+    Sub,
+    Mul,
+    Div,
+    Lt,
+    Lte,
+    Gt,
+    Gte,
+    Eq,
+    Neq,
+}
+
+#[derive(Debug, Copy, Clone)]
+enum Literal {
+    Int(i64),
+    Bool(bool),
+    Unit,
+}
+
+#[derive(Debug, Clone)]
+enum Value<'src> {
+    Int(i64),
+    Bool(bool),
+    Unit,
+    // closure name (if present), arg identifier, expr its pointing to
+    Closure(Option<Identifier>, Identifier, &'src Expr, Box<Env<'src>>)
+}
+
+#[derive(Debug, Clone, Default)]
+struct Env<'src> {
+    bindings: BTreeMap<Identifier, Value<'src>>,
+}
+
+
+
+fn walk<'src>(env: Env<'src>, expr: &'src Expr) -> Value<'src> {
+    match expr {
+        Expr::Lit(Literal::Int(i)) => Value::Int(*i),
+        Expr::Lit(Literal::Bool(b)) => Value::Bool(*b),
+        Expr::Lit(Literal::Unit) => Value::Unit,
+        Expr::Ident(var) => env.bindings.get(var).expect(format!("Unknown binding: {}", var).as_str()).clone(),
+        Expr::Let(binding, value, rest) => {
+            let mut new_env = env.clone();
+            let value = walk(env, value);
+            new_env.bindings.insert(binding.clone(), value);
+            walk(new_env, rest)
+        }
+        Expr::BinOp(op, lhs, rhs) => {
+            let left = walk(env.clone(), lhs);
+            let right = walk(env, rhs);
+            match (op, left, right) {
+                (Bop::Add, Value::Int(l), Value::Int(r)) => Value::Int(l + r),
+                (Bop::Sub, Value::Int(l), Value::Int(r)) => Value::Int(l - r),
+                (Bop::Mul, Value::Int(l), Value::Int(r)) => Value::Int(l * r),
+                (Bop::Div, Value::Int(l), Value::Int(r)) => Value::Int(l / r),
+                (Bop::Lt, Value::Int(l), Value::Int(r)) => Value::Bool(l < r),
+                (Bop::Lte, Value::Int(l), Value::Int(r)) => Value::Bool(l <= r),
+                (Bop::Gt, Value::Int(l), Value::Int(r)) => Value::Bool(l > r),
+                (Bop::Gte, Value::Int(l), Value::Int(r)) => Value::Bool(l >= r),
+                (Bop::Eq, Value::Int(l), Value::Int(r)) => Value::Bool(l == r),
+                (Bop::Neq, Value::Int(l), Value::Int(r)) => Value::Bool(l != r),
+                _ => panic!("Bad op types")
+            }
+        }
+        Expr::If(cond, then, else_) => {
+            let Value::Bool(c) = walk(env.clone(), cond) else { panic!("Can't do if on non-boolean") };
+            if c { walk(env, then) } else { walk(env, else_) }
+        }
+        Expr::Closure(name, arg, body) => {
+            Value::Closure(name.clone(), arg.clone(), body.as_ref(), Box::new(env))
+        }
+        Expr::App(func, arg) => {
+            // evaluate argument in current env
+            let arg_value = walk(env.clone(), arg);
+            // evaluate function in current env
+            let Value::Closure(name, arg_id, body, clos_env) = walk(env, func) else {
+                panic!("Can't apply to non closure")
+            };
+            let mut new_env = clos_env.clone();
+            if let Some(name) = name {
+                new_env.bindings.insert(name.clone(), Value::Closure(Some(name), arg_id.clone(), body, clos_env));
+            }
+            // bind argument
+            new_env.bindings.insert(arg_id, arg_value);
+            // evaluate function body
+            walk(*new_env, body)
+        }
+    }
+}
+
+
+
+
+fn main() {
+
+
+    //  let rec factorial n =
+    //      if n <= 1 then
+    //          1
+    //      else
+    //          n * factorial (n - 1)
+    //  in
+    //  let a = factorial 10 in
+    //  a
+
+    let ast = let_(
+        "factorial", func(Some("factorial"), "n",
+            if_(var("n").lte(num(1)),
+                num(1),
+                var("n").mul(
+                    app(
+                        var("factorial"),
+                        var("n").sub(num(1))
+                    )
+                )
+            )
+        ),
+        let_("a", app(var("factorial"), num(10)),
+             var("a")
+        )
+    );
+
+    println!("Ast: {:?}", ast);
+
+    let env = Env::default();
+    println!("{:?}", walk(env, &ast));
+}
